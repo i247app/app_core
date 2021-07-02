ec4b569ca06d3a5a5602dfda5f7d5e7c0e1f1215
HuyHT <huynhtuanhuy1996@gmail.com>
2021-07-02 14:32:21 +0700

    Code tweaks

--- lib/app_core.dart
+++ lib/app_core.dart
@@ -1,17 +1,17 @@
 library app_core;
 
-export 'package:app_core/model/chat.dart';
-export 'package:app_core/model/chat_member.dart';
-export 'package:app_core/model/chat_message.dart';
-export 'package:app_core/model/user.dart';
-export 'package:app_core/model/user_session.dart';
+// export 'package:app_core/model/chat.dart';
+// export 'package:app_core/model/chat_member.dart';
+// export 'package:app_core/model/chat_message.dart';
+// export 'package:app_core/model/user.dart';
+// export 'package:app_core/model/user_session.dart';
 export 'package:app_core/model/response/base_response.dart';
 export 'package:app_core/model/response/simple_response.dart';
 
 export 'package:app_core/network/tls_helper.dart';
 export 'package:app_core/helper/webrtc_helper.dart';
 
-export 'package:app_core/ui/chat/chatroom.dart';
+// export 'package:app_core/ui/chat/chatroom.dart';
 export 'package:app_core/ui/voip/simple_websocket.dart';
 
 class AppCore {}
