c55cf6e8349adda856d9fe8d95245e007582c26a
HuyHT <huynhtuanhuy1996@gmail.com>
2021-07-02 14:49:40 +0700

    Export helpers

--- lib/app_core.dart
+++ lib/app_core.dart
@@ -11,6 +11,14 @@ export 'package:app_core/model/response/simple_response.dart';
 export 'package:app_core/network/tls_helper.dart';
 export 'package:app_core/helper/webrtc_helper.dart';
 export 'package:app_core/helper/toast_helper.dart';
+export 'package:app_core/helper/util.dart';
+export 'package:app_core/helper/throttle_helper.dart';
+export 'package:app_core/helper/tablet_detector.dart';
+export 'package:app_core/helper/string_helper.dart';
+export 'package:app_core/helper/kcode.dart';
+export 'package:app_core/helper/locale_helper.dart';
+export 'package:app_core/helper/device_id_helper.dart';
+export 'package:app_core/helper/date_helper.dart';
 
 // export 'package:app_core/ui/chat/chatroom.dart';
 export 'package:app_core/ui/voip/simple_websocket.dart';
