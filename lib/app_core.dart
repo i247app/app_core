696a5ffbd1d62d0f02ef8612ad07d4c4899d0b37
HuyHT <huynhtuanhuy1996@gmail.com>
2021-07-02 09:59:46 +0700

    Export module

--- lib/app_core.dart
+++ lib/app_core.dart
@@ -5,6 +5,10 @@ export 'package:app_core/model/chat_member.dart';
 export 'package:app_core/model/chat_message.dart';
 export 'package:app_core/model/user.dart';
 export 'package:app_core/model/user_session.dart';
+export 'package:app_core/model/response/base_response.dart';
+export 'package:app_core/model/response/simple_response.dart';
+
+export 'package:app_core/network/tls_helper.dart';
 
 export 'package:app_core/ui/chat/chatroom.dart';
 
