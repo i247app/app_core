c3ebebdd1243a9b3b1f2585e26006f4476a4677a
HuyHT <huynhtuanhuy1996@gmail.com>
2021-07-02 14:42:49 +0700

    Port toast_helper

---
+++ lib/helper/toast_helper.dart
@@ -0,0 +1,39 @@
+import 'package:app_core/helper/host_config.dart';
+import 'package:app_core/header/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+abstract class ToastHelper {
+  /// Display a toast with standardized appearance
+  static Future<bool?> show(
+    String message, {
+    Color? textColor,
+    Color? backgroundColor,
+  }) {
+    try {
+      if (HostConfig.isReleaseMode) {
+        return Future.value(false);
+      } else {
+        return Fluttertoast.showToast(
+          msg: message,
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          textColor: textColor ?? Styles.extraDarkGrey,
+          fontSize: 16.0,
+          backgroundColor:
+              backgroundColor ?? Styles.extraLightGrey.withAlpha(0xBB),
+        );
+      }
+    } catch (e) {
+      return Future.value(false);
+    }
+  }
+
+  /// Display generic error message
+  static Future<bool?> error([String? message]) async =>
+      show(message ?? "An error occurred", textColor: Styles.colorError);
+
+  /// Display generic success message
+  static Future<bool?> success([String? message]) async =>
+      show(message ?? "Success", textColor: Styles.black);
+}
