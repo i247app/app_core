faba65ffbc5f1b85a28263a5f9dc4e367263e62a
HuyHT <huynhtuanhuy1996@gmail.com>
2021-07-02 14:22:06 +0700

    Export webrtc_helper

--- lib/app_core.dart
+++ lib/app_core.dart
@@ -9,6 +9,7 @@ export 'package:app_core/model/response/base_response.dart';
 export 'package:app_core/model/response/simple_response.dart';
 
 export 'package:app_core/network/tls_helper.dart';
+export 'package:app_core/helper/webrtc_helper.dart';
 
 export 'package:app_core/ui/chat/chatroom.dart';
 
