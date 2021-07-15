6abe67240403f1ce91adb9ece7658ad8ca620905
HuyHT <huynhtuanhuy1996@gmail.com>
2021-07-15 23:10:40 +0700

    Update biometric_helper.dart

--- lib/helper/biometric_helper.dart
+++ lib/helper/biometric_helper.dart
@@ -4,15 +4,15 @@ import 'package:local_auth/local_auth.dart';
 import 'package:app_core/helper/plugin_helper.dart';
 
 abstract class AppCoreBiometricHelper {
-  static final LocalAuthentication localAuth = LocalAuthentication();
+  final LocalAuthentication localAuth = LocalAuthentication();
 
-  static Future<bool> isAvailable() async {
+  Future<bool> isAvailable() async {
     bool canCheckBiometrics = await localAuth.canCheckBiometrics;
     final hasAvailableMethods = await localAuth.getAvailableBiometrics().then((r) => r.isNotEmpty);
     return canCheckBiometrics && hasAvailableMethods;
   }
 
-  static Future<bool> authenticate(String reason) async {
+  Future<bool> authenticate(String reason) async {
     if (Platform.isAndroid) {
       try {
         final response = await AppCorePluginHelper.biometricAuth(reason);
