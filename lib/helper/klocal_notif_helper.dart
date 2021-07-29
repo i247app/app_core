e8523352b58ccc6b25b3cb58d00eabf0590d22b9
Thanh Nguyen <thanhnd.ltt@gmail.com>
2021-07-29 12:26:29 +0700

    receive transparent icon

--- lib/helper/klocal_notif_helper.dart
+++ lib/helper/klocal_notif_helper.dart
@@ -16,7 +16,7 @@ import 'dart:math';
 
 abstract class KLocalNotifHelper {
   static const bool BLOCKED_BANNERS_AS_TOAST = false;
-
+  static String? transparentIcon;
   static FlutterLocalNotificationsPlugin? _flutterLocalNotificationsPlugin;
 
   // TODO replace with smarter system
@@ -52,12 +52,12 @@ abstract class KLocalNotifHelper {
   static void _logBlockDepth(String app) => print(
       "LocalNotifHelper :: banner block depth - ${_blockedBannersDepth[app]}");
 
-  static Future<void> setupLocalNotifications(
-      KREMManager dispatcher, String transparentIcon) async {
+  static Future<void> setupLocalNotifications(KREMManager dispatcher) async {
     print("fcm_helper => setupLocalNotifications fired");
 
     _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    final android = AndroidInitializationSettings(transparentIcon);
+    final android =
+        AndroidInitializationSettings(KLocalNotifHelper.transparentIcon!);
     final ios = IOSInitializationSettings();
     final platform = InitializationSettings(android: android, iOS: ios);
 
