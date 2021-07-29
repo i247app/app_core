82dcf0d8cdac6919828e958d459a944ee5a451a9
Thanh Nguyen <thanhnd.ltt@gmail.com>
2021-07-29 09:43:59 +0700

    Format KREM

--- lib/rem/krem.dart
+++ lib/rem/krem.dart
@@ -3,10 +3,16 @@ import 'package:flutter/widgets.dart';
 typedef KREMAction = Future Function(NavigatorState);
 
 abstract class KREM {
+  // common
   static const String APP = "app";
-  static const String AMP = "amp";
-  static const String PROMO = "promo";
   static const String CHAT = "chat";
   static const String USER = "user";
   static const String P2P = "p2p";
+
+  // chao
+  static const String AMP = "amp";
+  static const String PROMO = "promo";
+
+  // schoolbird
+  static const GIG = "gig";
 }
