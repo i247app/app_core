e343f1f05b5246885ab761255900122d3c75d589
alex-heritier <alex.heritier@gmail.com>
2021-08-15 16:22:18 -0700

    updated globals

--- lib/helper/kglobals.dart
+++ lib/helper/kglobals.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/widgets.dart';
 
 final GlobalKey<NavigatorState> kNavigatorKey = GlobalKey<NavigatorState>();
-// final RouteObserver<PageRoute> chaoRouteObserver = RouteObserver<PageRoute>();
+final RouteObserver<PageRoute> kRouteObserver = RouteObserver<PageRoute>();
