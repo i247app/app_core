c145518785b3f05efa612c65075aee881e9ee8ad
alex-heritier <alex.heritier@gmail.com>
2021-08-16 20:45:30 -0700

    code cleanup in banner overlay

--- lib/ui/widget/kflash_banner_overlay.dart
+++ lib/ui/widget/kflash_banner_overlay.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:app_core/helper/kflash_helper.dart';
@@ -15,13 +13,9 @@ class _KFlashBannerOverlayState extends State<KFlashBannerOverlay>
   late final AnimationController slideAnimationController;
   late final Animation<Offset> slideAnimation;
 
-  final int generatorCount = 5;
-  final Duration confettiDelay = Duration(milliseconds: 500);
-  final Duration messageDisplay = Duration(milliseconds: 4500);
-  final Duration messageAnimation = Duration(milliseconds: 1250);
-
-  int get particleCount =>
-      max(1, KFlashHelper.confettiCount ~/ this.generatorCount);
+  final Duration delayDuration = Duration(milliseconds: 500);
+  final Duration displayDuration = Duration(milliseconds: 4500);
+  final Duration animationDuration = Duration(milliseconds: 1250);
 
   String get message => KFlashHelper.flashController.value.media ?? "";
 
@@ -32,7 +26,7 @@ class _KFlashBannerOverlayState extends State<KFlashBannerOverlay>
     super.initState();
 
     this.slideAnimationController = AnimationController(
-      duration: this.messageAnimation,
+      duration: this.animationDuration,
       vsync: this,
     );
     this.slideAnimation = Tween<Offset>(
@@ -43,32 +37,34 @@ class _KFlashBannerOverlayState extends State<KFlashBannerOverlay>
       curve: Curves.easeInCubic,
     ));
 
-    KFlashHelper.confettiController.addListener(confettiHelperListener);
+    KFlashHelper.flashController.addListener(confettiHelperListener);
   }
 
   @override
   void dispose() {
     this.slideAnimationController.dispose();
-    KFlashHelper.confettiController.removeListener(confettiHelperListener);
+    KFlashHelper.flashController.removeListener(confettiHelperListener);
     super.dispose();
   }
 
   void confettiHelperListener() {
     this.setState(() => this.isShowMessage = true);
     Future.delayed(
-        this.messageDisplay, () => setState(() => this.isShowMessage = false));
+      this.displayDuration,
+      () => setState(() => this.isShowMessage = false),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final animatedLayer = AnimatedOpacity(
-      duration: this.confettiDelay,
+      duration: this.delayDuration,
       opacity: this.isShowMessage ? 1 : 0,
       child: Container(color: KStyles.black.withOpacity(0.5)),
     );
 
     final messageLayer = AnimatedOpacity(
-      duration: this.confettiDelay,
+      duration: this.delayDuration,
       opacity: this.isShowMessage ? 1 : 0,
       child: SlideTransition(
         position: this.slideAnimation,
