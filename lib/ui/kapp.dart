815044d2aa0006b9640f5da81a18a29ebb80b4df
alex-heritier <alex.heritier@gmail.com>
2021-08-18 17:13:25 -0700

    ported over KIcon + kapp updates

--- lib/ui/kapp.dart
+++ lib/ui/kapp.dart
@@ -1,66 +1,83 @@
-import 'package:app_core/helper/kglobals.dart' as global;
+import 'dart:async';
+
+import 'package:app_core/helper/koverlay_helper.dart';
 import 'package:app_core/style/ktheme.dart';
 import 'package:app_core/style/kstyle_manager.dart';
+import 'package:app_core/ui/kicon/kicon_manager.dart';
 import 'package:app_core/ui/widget/kembed_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class KApp extends StatelessWidget {
-  final Widget body;
+  final Widget home;
   final TextStyle defaultTextStyle;
   final bool isEmbed;
   final String title;
   final KTheme kTheme;
-  final GlobalKey<NavigatorState>? navigatorKey;
-  final List<NavigatorObserver>? navigatorObservers;
-
-  static KTheme get defaultTheme => KTheme(
-        palettePrimary: Color(0xff79AF2C),
-        paletteSecondary: Color(0xffFBA922),
-        paletteSupport: Color(0xffA3D0C1),
-        paletteButtonText: Colors.white,
-        palette4: Color(0xffFF8370),
-        palette5: Color(0xffCEB27C),
-        yes: Color(0xff79AF2C),
-        no: Colors.red,
-        themeData: ThemeData(
-          primarySwatch: Colors.lightGreen,
-          backgroundColor: Colors.white,
-          brightness: Brightness.light,
-        ),
-        child: Container(),
-      );
+  final GlobalKey<NavigatorState> navigatorKey;
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  final List<NavigatorObserver> navigatorObservers;
+  final Map<dynamic, KIconProvider> iconSet;
+  final Completer? initializer;
 
   const KApp({
-    required this.body,
+    required this.home,
     required this.defaultTextStyle,
     required this.kTheme,
+    required this.navigatorKey,
+    required this.scaffoldKey,
+    required this.navigatorObservers,
     this.isEmbed = false,
     this.title = '',
-    this.navigatorKey,
-    this.navigatorObservers,
+    this.iconSet = const {},
+    this.initializer,
   });
 
   @override
   Widget build(BuildContext context) {
-    final app = MaterialApp(
-      title: this.title,
-      navigatorKey: this.navigatorKey ?? global.kNavigatorKey,
-      debugShowCheckedModeBanner: false,
-      navigatorObservers: this.navigatorObservers ?? [],
-      theme: this.kTheme.themeData,
-      home: this.body,
-    );
-
-    return KStyleManager(
+    final innerApp = KStyleManager(
       theme: this.kTheme,
-      child: KEmbedManager(
-        isEmbed: this.isEmbed,
-        child: DefaultTextStyle(
-          style: this.defaultTextStyle,
-          child: app,
+      child: KIconManager(
+        iconSet: this.iconSet,
+        child: KEmbedManager(
+          isEmbed: this.isEmbed,
+          child: DefaultTextStyle(
+            style: this.defaultTextStyle,
+            child: MaterialApp(
+              title: this.title,
+              navigatorKey: this.navigatorKey,
+              debugShowCheckedModeBanner: false,
+              navigatorObservers: this.navigatorObservers,
+              theme: this.kTheme.themeData,
+              home: this.home,
+            ),
+          ),
         ),
       ),
     );
+
+    final rawInnerAppWithOverlay = Stack(
+      fit: StackFit.expand,
+      children: [innerApp, KOverlayHelper.build()],
+    );
+
+    final innerAppWithOverlay = this.initializer == null
+        ? rawInnerAppWithOverlay
+        : FutureBuilder(
+            future: this.initializer!.future,
+            builder: (_, snapshot) =>
+                !snapshot.hasData ? Container() : rawInnerAppWithOverlay,
+          );
+
+    final masterApp = MaterialApp(
+      debugShowCheckedModeBanner: false,
+      builder: (_, __) => Scaffold(
+        resizeToAvoidBottomInset: false,
+        key: this.scaffoldKey,
+        body: innerAppWithOverlay,
+      ),
+    );
+
+    return masterApp;
   }
 }
---
+++ lib/ui/kicon/kicon.dart
@@ -0,0 +1,44 @@
+import 'package:app_core/ui/kicon/kicon_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class KIcon extends StatelessWidget {
+  final dynamic iconID;
+  final Color? color;
+
+  double get width => 32;
+
+  double get height => 32;
+
+  const KIcon(this.iconID, {this.color});
+
+  Widget buildRaw(ctx) => getProvider(ctx)!.resource.runtimeType == IconData
+      ? Icon(
+          getProvider(ctx)!.resource,
+          color: this.color,
+        )
+      : Image.asset(
+          getProvider(ctx)!.resource,
+          fit: BoxFit.contain,
+        );
+
+  KIconProvider? getProvider(ctx) => KIconManager.of(ctx).iconSet[this.iconID];
+
+  @override
+  Widget build(BuildContext context) {
+    final raw = getProvider(context) == null
+        ? Icon(
+            Icons.error,
+            color: this.color,
+          )
+        : buildRaw(context);
+
+    final body = Container(
+      // width: this.width,
+      // height: this.height,
+      child: raw,
+    );
+
+    return body;
+  }
+}
