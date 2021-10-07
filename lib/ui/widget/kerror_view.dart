acfaef8bb7bdd32ba4da86c82e6f4751683c499b
alex-heritier <alex.heritier@gmail.com>
2021-10-07 16:59:05 -0700

    updated kerror view

--- lib/ui/widget/kerror_view.dart
+++ lib/ui/widget/kerror_view.dart
@@ -1,4 +1,5 @@
 import 'package:app_core/header/kstyles.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class KErrorView extends StatelessWidget {
@@ -8,10 +9,20 @@ class KErrorView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final body = Text(
+    final label = Text(
       "Oops, an error occurred! We'll fix this soon.",
       textAlign: TextAlign.center,
       style: KStyles.largeText,
+    );
+
+    final body = Container(
+      padding: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(2),
+        border: Border.all(color: Colors.red),
+        color: Colors.red.withOpacity(0.5),
+      ),
+      child: label,
     );
 
     return body;
