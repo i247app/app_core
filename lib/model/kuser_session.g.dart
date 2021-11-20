ddcf5d2f56ea07361a4a25a2a9f259794269bac7
alex-heritier <alex.heritier@gmail.com>
2021-11-19 19:57:31 -0800

    added the kuser session g file

---
+++ lib/model/kuser_session.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'kuser_session.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+KUserSession _$KUserSessionFromJson(Map<String, dynamic> json) => KUserSession()
+  ..ktoken = json['ktoken'] as String?
+  ..puid = json['puid'] as String?
+  ..googleMapAPIKey = json['googleMapAPIKey'] as String?
+  ..hostData = json['hostData'] == null
+      ? null
+      : KSystemHostData.fromJson(json['hostData'] as Map<String, dynamic>);
+
+Map<String, dynamic> _$KUserSessionToJson(KUserSession instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('ktoken', instance.ktoken);
+  writeNotNull('puid', instance.puid);
+  writeNotNull('googleMapAPIKey', instance.googleMapAPIKey);
+  writeNotNull('hostData', instance.hostData?.toJson());
+  return val;
+}
