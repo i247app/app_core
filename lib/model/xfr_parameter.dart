5e66fcce24c859e332cbfaae0d37f2196ce55ea2
alex-heritier <alex.heritier@gmail.com>
2021-11-26 23:04:44 -0800

    xfr parameter

---
+++ lib/model/xfr_parameter.dart
@@ -0,0 +1,8 @@
+class XFRParameter {
+  final String? fone;
+  final String? puid;
+  final String? promotionType;
+  final String? promotionValue;
+
+  XFRParameter({this.promotionType, this.promotionValue, this.fone, this.puid});
+}
