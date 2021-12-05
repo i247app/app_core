e81a249c095a85f0b9b09f31a09430563a5f8fb3
alex-heritier <alex.heritier@gmail.com>
2021-12-04 17:14:59 -0800

    kextensions list unique

--- lib/header/kextensions.dart
+++ lib/header/kextensions.dart
@@ -1,6 +1,8 @@
+import 'package:collection/src/iterable_extensions.dart';
 import 'package:flutter/widgets.dart';
 
 extension KList<T> on List<T> {
+  /// Insert an element 't' between each list item
   List<T> intersperse(T t, {bool addToEnd = false}) {
     final result = <T>[];
     for (int i = 0; i < this.length; i++) {
@@ -9,6 +11,21 @@ extension KList<T> on List<T> {
       if (!isLastItem || addToEnd) result.add(t);
     }
     return result;
+  }
+
+  /// Remove duplicates
+  Iterable<T> unique(dynamic Function(T) extractor) {
+    // rawTutorGigs
+    //     ?.map((tg) => tg.gigID) // get all current IDs
+    //     .map((id) => rawTutorGigs
+    //     ?.firstWhereOrNull((rtg) => rtg.gigID == id)) // get 1 gig per ID
+    //     .where((tg) => tg?.gigStatus != null) // filter out null gigs
+    //     .cast<TutorGig>()
+    //     .toList()
+    return map((e) => extractor(e))
+        .map((e1) => firstWhereOrNull((e2) => e1 == extractor(e2)))
+        .where((e) => e != null)
+        .cast<T>();
   }
 }
 
