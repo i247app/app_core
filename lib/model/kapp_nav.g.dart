7547399e0634aa37deb1b7a384a68802040334e0
rkl555 <85212537+rkl550@users.noreply.github.com>
2022-01-13 01:50:46 +0700

    ver 1.0.99

--- lib/model/kapp_nav.g.dart
+++ lib/model/kapp_nav.g.dart
@@ -7,15 +7,16 @@ part of 'kapp_nav.dart';
 // **************************************************************************
 
 KAppNav _$KAppNavFromJson(Map<String, dynamic> json) => KAppNav()
+  ..chatAppMode = zzz_appNavTryAtoi(json['chatAppMode'])
+  ..payAppMode = zzz_appNavTryAtoi(json['payAppMode'])
+  ..feedAppMode = zzz_appNavTryAtoi(json['feedAppMode'])
+  ..rewardAppMode = zzz_appNavTryAtoi(json['rewardAppMode'])
+  ..ampAppMode = zzz_appNavTryAtoi(json['ampAppMode'])
+  ..splashMode = zzz_appNavTryAtoi(json['splashMode'])
   ..studyAppMode = zzz_appNavTryAtoi(json['studyAppMode'])
   ..classAppMode = zzz_appNavTryAtoi(json['classAppMode'])
   ..universityAppMode = zzz_appNavTryAtoi(json['uniAppMode'])
-  ..gigAppMode = zzz_appNavTryAtoi(json['gigAppMode'])
-  ..chatAppMode = zzz_appNavTryAtoi(json['chatAppMode'])
-  ..rewardAppMode = zzz_appNavTryAtoi(json['rewardAppMode'])
-  ..payAppMode = zzz_appNavTryAtoi(json['payAppMode'])
-  ..ampAppMode = zzz_appNavTryAtoi(json['ampAppMode'])
-  ..splashMode = zzz_appNavTryAtoi(json['splashMode']);
+  ..gigAppMode = zzz_appNavTryAtoi(json['gigAppMode']);
 
 Map<String, dynamic> _$KAppNavToJson(KAppNav instance) {
   final val = <String, dynamic>{};
@@ -26,14 +27,15 @@ Map<String, dynamic> _$KAppNavToJson(KAppNav instance) {
     }
   }
 
+  writeNotNull('chatAppMode', zzz_itoa(instance.chatAppMode));
+  writeNotNull('payAppMode', zzz_itoa(instance.payAppMode));
+  writeNotNull('feedAppMode', zzz_itoa(instance.feedAppMode));
+  writeNotNull('rewardAppMode', zzz_itoa(instance.rewardAppMode));
+  writeNotNull('ampAppMode', zzz_itoa(instance.ampAppMode));
+  writeNotNull('splashMode', zzz_itoa(instance.splashMode));
   writeNotNull('studyAppMode', zzz_itoa(instance.studyAppMode));
   writeNotNull('classAppMode', zzz_itoa(instance.classAppMode));
   writeNotNull('uniAppMode', zzz_itoa(instance.universityAppMode));
   writeNotNull('gigAppMode', zzz_itoa(instance.gigAppMode));
-  writeNotNull('chatAppMode', zzz_itoa(instance.chatAppMode));
-  writeNotNull('rewardAppMode', zzz_itoa(instance.rewardAppMode));
-  writeNotNull('payAppMode', zzz_itoa(instance.payAppMode));
-  writeNotNull('ampAppMode', zzz_itoa(instance.ampAppMode));
-  writeNotNull('splashMode', zzz_itoa(instance.splashMode));
   return val;
 }
