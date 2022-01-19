631b29ef7009c6ad0f32a4cf94403054166ab30f
HuyHT <huynhtuanhuy1996@gmail.com>
2022-01-19 16:27:47 +0700

    Added egg hero game intro

--- lib/ui/hero/khero_speech_tap_game.dart
+++ lib/ui/hero/khero_speech_tap_game.dart
@@ -9,6 +9,7 @@ import 'package:app_core/model/kgame.dart';
 import 'package:app_core/model/khero.dart';
 import 'package:app_core/model/kquestion.dart';
 import 'package:app_core/model/kgame_score.dart';
+import 'package:app_core/ui/hero/widget/kegg_hero_intro.dart';
 import 'package:app_core/ui/hero/widget/khero_game_count_down_intro.dart';
 import 'package:app_core/ui/hero/widget/khero_game_end.dart';
 import 'package:app_core/ui/hero/widget/khero_game_highscore_dialog.dart';
@@ -51,6 +52,7 @@ class _KHeroSpeechTapGameState extends State<KHeroSpeechTapGame> {
   int totalLevel = 4;
   int currentLevel = 0;
   bool isShowEndLevel = false;
+  bool isShowIntro = true;
 
   KGameScore? score;
   KGame? game = null;
@@ -172,87 +174,49 @@ class _KHeroSpeechTapGameState extends State<KHeroSpeechTapGame> {
             padding: EdgeInsets.symmetric(vertical: 20),
             child: !isLoaded || game == null
                 ? Container()
-                : SafeArea(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Padding(
-                        //   padding: EdgeInsets.only(
-                        //     left: 15,
-                        //   ),
-                        //   child: GestureDetector(
-                        //     onTap: () {
-                        //       Navigator.of(context).pop();
-                        //     },
-                        //     child: Icon(
-                        //       Icons.arrow_back_ios,
-                        //       color: Colors.white,
-                        //     ),
-                        //   ),
-                        // ),
-                        Expanded(
-                          child: KSpeechTapGameScreen(
-                            hero: widget.hero,
-                            totalLevel: totalLevel,
-                            isShowEndLevel: isShowEndLevel,
-                            questions: questions,
-                            level: currentLevel,
-                            isLoaded: isLoaded,
-                            onFinishLevel: (level, score, canAdvance) {
-                              if (!canAdvance) {
-                                this.showHeroGameLevelOverlay(() {
-                                  this.setState(() {
-                                    this.isShowEndLevel = false;
-                                  });
-                                  if (this.overlayID != null) {
-                                    KOverlayHelper.removeOverlay(
-                                        this.overlayID!);
-                                    this.overlayID = null;
-                                  }
-                                }, canAdvance: canAdvance);
-                                return;
-                              }
-
-                              this.setState(() {
-                                this.score = KGameScore()
-                                  ..game = GAME_ID
-                                  ..avatarURL = KSessionData.me!.avatarURL
-                                  ..kunm = KSessionData.me!.kunm
-                                  ..level = "$level"
-                                  ..score = "$score";
-                              });
-                              if (level < totalLevel) {
-                                // if (!isHaveWrongAnswer) {
-                                //   this.setState(() {
-                                //     this.levelHighscores.add(score);
-                                //   });
-                                // }
-                                this.showHeroGameLevelOverlay(() {
-                                  if (this.overlayID != null) {
-                                    KOverlayHelper.removeOverlay(
-                                        this.overlayID!);
-                                    this.overlayID = null;
-                                  }
-                                  this.showHeroGameHighscoreOverlay(() {
-                                    this.setState(() {
-                                      this.isShowEndLevel = false;
-                                    });
-                                    if (this.overlayID != null) {
-                                      KOverlayHelper.removeOverlay(
-                                          this.overlayID!);
-                                      this.overlayID = null;
-                                    }
-                                  });
-                                }, canAdvance: canAdvance);
-                              } else {
-                                this.showHeroGameEndOverlay(
-                                  () {
-                                    if (this.overlayID != null) {
-                                      KOverlayHelper.removeOverlay(
-                                          this.overlayID!);
-                                      this.overlayID = null;
-                                    }
-                                    this.showHeroGameHighscoreOverlay(() {
+                : (this.isShowIntro
+                    ? Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          if (this.isShowIntro) ...[
+                            KEggHeroIntro(
+                                onFinish: () =>
+                                    setState(() => this.isShowIntro = false)),
+                            GestureDetector(
+                                onTap: () =>
+                                    setState(() => this.isShowIntro = false)),
+                          ],
+                        ],
+                      )
+                    : SafeArea(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // Padding(
+                            //   padding: EdgeInsets.only(
+                            //     left: 15,
+                            //   ),
+                            //   child: GestureDetector(
+                            //     onTap: () {
+                            //       Navigator.of(context).pop();
+                            //     },
+                            //     child: Icon(
+                            //       Icons.arrow_back_ios,
+                            //       color: Colors.white,
+                            //     ),
+                            //   ),
+                            // ),
+                            Expanded(
+                              child: KSpeechTapGameScreen(
+                                hero: widget.hero,
+                                totalLevel: totalLevel,
+                                isShowEndLevel: isShowEndLevel,
+                                questions: questions,
+                                level: currentLevel,
+                                isLoaded: isLoaded,
+                                onFinishLevel: (level, score, canAdvance) {
+                                  if (!canAdvance) {
+                                    this.showHeroGameLevelOverlay(() {
                                       this.setState(() {
                                         this.isShowEndLevel = false;
                                       });
@@ -261,24 +225,76 @@ class _KHeroSpeechTapGameState extends State<KHeroSpeechTapGame> {
                                             this.overlayID!);
                                         this.overlayID = null;
                                       }
-                                    });
-                                  },
-                                );
-                              }
-                            },
-                            onChangeLevel: (level) {
-                              this.setState(
-                                () {
-                                  this.currentLevel = level;
+                                    }, canAdvance: canAdvance);
+                                    return;
+                                  }
+
+                                  this.setState(() {
+                                    this.score = KGameScore()
+                                      ..game = GAME_ID
+                                      ..avatarURL = KSessionData.me!.avatarURL
+                                      ..kunm = KSessionData.me!.kunm
+                                      ..level = "$level"
+                                      ..score = "$score";
+                                  });
+                                  if (level < totalLevel) {
+                                    // if (!isHaveWrongAnswer) {
+                                    //   this.setState(() {
+                                    //     this.levelHighscores.add(score);
+                                    //   });
+                                    // }
+                                    this.showHeroGameLevelOverlay(() {
+                                      if (this.overlayID != null) {
+                                        KOverlayHelper.removeOverlay(
+                                            this.overlayID!);
+                                        this.overlayID = null;
+                                      }
+                                      this.showHeroGameHighscoreOverlay(() {
+                                        this.setState(() {
+                                          this.isShowEndLevel = false;
+                                        });
+                                        if (this.overlayID != null) {
+                                          KOverlayHelper.removeOverlay(
+                                              this.overlayID!);
+                                          this.overlayID = null;
+                                        }
+                                      });
+                                    }, canAdvance: canAdvance);
+                                  } else {
+                                    this.showHeroGameEndOverlay(
+                                      () {
+                                        if (this.overlayID != null) {
+                                          KOverlayHelper.removeOverlay(
+                                              this.overlayID!);
+                                          this.overlayID = null;
+                                        }
+                                        this.showHeroGameHighscoreOverlay(() {
+                                          this.setState(() {
+                                            this.isShowEndLevel = false;
+                                          });
+                                          if (this.overlayID != null) {
+                                            KOverlayHelper.removeOverlay(
+                                                this.overlayID!);
+                                            this.overlayID = null;
+                                          }
+                                        });
+                                      },
+                                    );
+                                  }
                                 },
-                              );
-                              this.loadGame();
-                            },
-                          ),
+                                onChangeLevel: (level) {
+                                  this.setState(
+                                    () {
+                                      this.currentLevel = level;
+                                    },
+                                  );
+                                  this.loadGame();
+                                },
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
+                      )),
           ),
         ),
       ],
@@ -570,7 +586,8 @@ class KSpeechTapGameScreenState extends State<KSpeechTapGameScreen>
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.paused && !this.isPause) showPauseDialog();
+    if (state == AppLifecycleState.paused && !this.isPause)
+      showPauseDialog();
     else if (state == AppLifecycleState.resumed && this.isPause) resumeGame();
   }
 
