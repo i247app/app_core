278d8ae12c71495bc5144b25e3d68bb6c268d972
rkl555 <85212537+rkl550@users.noreply.github.com>
2022-03-09 01:50:24 +0700

    formatting

--- lib/helper/kapp_nav_helper.dart
+++ lib/helper/kapp_nav_helper.dart
@@ -3,22 +3,30 @@ import 'package:app_core/app_core.dart';
 abstract class KAppNavHelper {
   // core
   static int get chat => KSessionData.appNav?.chatAppMode ?? KAppNav.OFF;
+
   static int get pay => KSessionData.appNav?.payAppMode ?? KAppNav.OFF;
+
   static int get feed => KSessionData.appNav?.feedAppMode ?? KAppNav.OFF;
 
   // Chao
   static int get amp => KSessionData.appNav?.ampAppMode ?? KAppNav.OFF;
+
   static int get reward => KSessionData.appNav?.rewardAppMode ?? KAppNav.OFF;
+
   static int get splash => KSessionData.appNav?.splashMode ?? KAppNav.OFF;
 
   // Schoolbird
   static int get gig => KSessionData.appNav?.gigAppMode ?? KAppNav.OFF;
+
   static int get study => KSessionData.appNav?.studyAppMode ?? KAppNav.OFF;
+
   static int get classes => KSessionData.appNav?.classAppMode ?? KAppNav.OFF;
+
   static int get university =>
       KSessionData.appNav?.universityAppMode ?? KAppNav.OFF;
 
   static int get hero => KSessionData.appNav?.heroAppMode ?? KAppNav.OFF;
+
   static int get headstart =>
       KSessionData.appNav?.headstartAppMode ?? KAppNav.OFF;
 }
