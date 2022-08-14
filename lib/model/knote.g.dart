c9ed7acb0992ed20a26ac7155d3d52f8d492c2a3
alex-heritier <alex.heritier@gmail.com>
2022-08-14 12:33:38 +0700

    g file

--- lib/model/knote.g.dart
+++ lib/model/knote.g.dart
@@ -7,6 +7,21 @@ part of 'knote.dart';
 // **************************************************************************
 
 KNote _$KNoteFromJson(Map<String, dynamic> json) => KNote()
+  ..kdomain = json['domain'] as String?
+  ..kreportFlag = json['reportFlag'] as String?
+  ..knote = json['note'] as String?
+  ..kstatusCode = json['statusCode'] as String?
+  ..kcreateID = json['createID'] as String?
+  ..kcreateDate = zzz_str2Date(json['createDate'] as String?)
+  ..kmodifyID = json['modifyID'] as String?
+  ..kmodifyDate = zzz_str2Date(json['modifyDate'] as String?)
+  ..kisValid = json['isValid'] as String?
+  ..korderBy = json['orderBy'] as String?
+  ..klimit = json['limit'] as String?
+  ..koffset = json['offset'] as String?
+  ..kstatus = json['kstatus'] as String?
+  ..kmessage = json['kmessage'] as String?
+  ..kcount = json['kcount'] as String?
   ..id = json['id'] as String?
   ..noteID = json['noteID'] as String?
   ..action = json['action'] as String?
@@ -31,6 +46,21 @@ Map<String, dynamic> _$KNoteToJson(KNote instance) {
     }
   }
 
+  writeNotNull('domain', instance.kdomain);
+  writeNotNull('reportFlag', instance.kreportFlag);
+  writeNotNull('note', instance.knote);
+  writeNotNull('statusCode', instance.kstatusCode);
+  writeNotNull('createID', instance.kcreateID);
+  writeNotNull('createDate', zzz_date2Str(instance.kcreateDate));
+  writeNotNull('modifyID', instance.kmodifyID);
+  writeNotNull('modifyDate', zzz_date2Str(instance.kmodifyDate));
+  writeNotNull('isValid', instance.kisValid);
+  writeNotNull('orderBy', instance.korderBy);
+  writeNotNull('limit', instance.klimit);
+  writeNotNull('offset', instance.koffset);
+  writeNotNull('kstatus', instance.kstatus);
+  writeNotNull('kmessage', instance.kmessage);
+  writeNotNull('kcount', instance.kcount);
   writeNotNull('id', instance.id);
   writeNotNull('noteID', instance.noteID);
   writeNotNull('action', instance.action);
