c07a7ca709b9938bc8774c1bc4fe7c0aca6b8c00
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2022-10-27 22:40:57 +0700

    Added egg hatch after game intro

--- lib/ui/hero/widget/kegg_hero_intro_backup.dart
+++ lib/ui/hero/widget/kegg_hero_intro_backup.dart
@@ -9,39 +9,32 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:app_core/header/kassets.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:vector_math/vector_math_64.dart' as Vector;
 
-class KEggHeroIntro extends StatefulWidget {
+class KEggHeroIntroBackup extends StatefulWidget {
   final Function? onFinish;
 
-  const KEggHeroIntro({this.onFinish});
+  const KEggHeroIntroBackup({this.onFinish});
 
   @override
   _KEggHeroIntroState createState() => _KEggHeroIntroState();
 }
 
-class _KEggHeroIntroState extends State<KEggHeroIntro>
+class _KEggHeroIntroState extends State<KEggHeroIntroBackup>
     with TickerProviderStateMixin {
   Completer<AudioPlayer> cAudioPlayer = Completer();
   Completer<AudioPlayer> cBackgroundAudioPlayer = Completer();
   String? correctAudioFileUri;
   String? introAudioFileUri;
 
-  late Animation<Offset> _bouncingAnimation, _adultBouncingAnimation;
-  late Animation<double> _adultShakeAnimation,
-      _shakeTheTopAnimation,
+  late Animation<Offset> _bouncingAnimation;
+  late Animation<double> _shakeTheTopAnimation,
       _barrelMovingAnimation,
       _barrelHeroMovingAnimation;
-  late AnimationController _adultShakeAnimationController,
-      _shakeTheTopAnimationController,
-      _adultBouncingAnimationController,
+  late AnimationController _shakeTheTopAnimationController,
       _bouncingAnimationController,
       _barrelMovingAnimationController,
       _barrelHeroMovingAnimationController,
       _barrelHeroSpinAnimationController;
-
-  final Duration adultBouncingDuration = Duration(milliseconds: 750);
-  final Duration adultShakeDuration = Duration(milliseconds: 750);
 
   double initialPos = 1;
   double height = 0;
@@ -51,10 +44,8 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
   Timer? _timer;
   double heroHeight = 90;
   double heroWidth = 90;
-  bool showEgg = true;
-  bool showAdult = false;
+  bool isShooting = false;
   int eggBreakStep = 0;
-  String? heroUrl;
 
   int introShakeTime = 1;
 
@@ -62,40 +53,7 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
   void initState() {
     super.initState();
 
-    List<String> heroUrls = [
-      "https://pix1.s3.us-west-1.amazonaws.com/hero/tofu_chan_k1.png",
-      "https://pix1.s3.us-west-1.amazonaws.com/hero/duriman_k1.png",
-      "https://pix1.s3.us-west-1.amazonaws.com/hero/midori_k1.png",
-      "https://pix1.s3.us-west-1.amazonaws.com/hero/midori_k1.png",
-      "https://pix1.s3.us-west-1.amazonaws.com/hero/midori_k1.png",
-    ];
-    heroUrls.shuffle();
-    heroUrl = heroUrls.first;
-
     loadAudioAsset();
-
-    _adultBouncingAnimationController =
-        AnimationController(vsync: this, duration: adultBouncingDuration)
-          ..addListener(() => setState(() {}));
-    _adultBouncingAnimation = Tween(begin: Offset(0, 0), end: Offset(0, -10.0))
-        .animate(_adultBouncingAnimationController);
-
-    this._adultShakeAnimationController = AnimationController(
-      vsync: this,
-      duration: adultShakeDuration,
-    )
-      ..addListener(() => setState(() {}))
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          Future.delayed(Duration(milliseconds: 500), () {
-            if (this.widget.onFinish != null) this.widget.onFinish!();
-          });
-        }
-      });
-    this._adultShakeAnimation = Tween<double>(
-      begin: 50.0,
-      end: 120.0,
-    ).animate(_adultShakeAnimationController);
 
     _bouncingAnimationController =
         AnimationController(vsync: this, duration: Duration(milliseconds: 100))
@@ -104,7 +62,6 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
             if (status == AnimationStatus.completed) {
               _bouncingAnimationController.reverse();
             } else if (status == AnimationStatus.dismissed) {
-              print("start rolling");
               this._barrelHeroMovingAnimationController.reverse();
               this._barrelHeroSpinAnimationController.repeat();
             }
@@ -129,9 +86,6 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
                 final ap = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
                 ap.play(correctAudioFileUri ?? "", isLocal: true);
                 cAudioPlayer.complete(ap);
-                this.setState(() {
-                  this.eggBreakStep = 1;
-                });
               } catch (e) {}
             }
             _shakeTheTopAnimationController.reverse();
@@ -189,7 +143,7 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
         } else if (mounted && status == AnimationStatus.dismissed) {
           this._barrelHeroSpinAnimationController.stop();
           this._barrelHeroSpinAnimationController.reset();
-          // if (this.widget.onFinish != null) this.widget.onFinish!();
+          if (this.widget.onFinish != null) this.widget.onFinish!();
         }
       });
     _barrelHeroMovingAnimation = new Tween(
@@ -228,10 +182,7 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
       ap.stop();
       ap.dispose();
     });
-    _adultShakeAnimationController.dispose();
     _shakeTheTopAnimationController.dispose();
-    _adultBouncingAnimationController.dispose();
-    _bouncingAnimationController.dispose();
     _barrelMovingAnimationController.dispose();
     _barrelHeroMovingAnimationController.dispose();
     _barrelHeroSpinAnimationController.dispose();
@@ -264,37 +215,9 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
     } catch (e) {}
   }
 
-  Vector.Vector3 adultShakeTransformValue() {
-    final progress = this._adultShakeAnimationController.value;
-    final offset = Math.sin(progress * Math.pi * 8.0);
-    return Vector.Vector3(offset * 8.0, 0.0, 0.0);
-  }
-
   @override
   Widget build(BuildContext context) {
-    final adultView = AnimatedOpacity(
-      duration: Duration(milliseconds: 0),
-      opacity: this.showAdult ? 1.0 : 0.0,
-      child: this.showAdult && heroUrl != null
-          ? Transform.scale(
-              alignment: Alignment.bottomCenter,
-              scale: 0.5,
-              child: KImageAnimation(
-                imageUrls: [heroUrl!],
-                animationType: KImageAnimationType.TINY_ZOOM_SHAKE,
-                maxLoop: 1,
-                onFinish: () => Future.delayed(
-                  Duration(milliseconds: 1000),
-                  () {
-                    if (widget.onFinish != null) widget.onFinish!();
-                  },
-                ),
-              ),
-            )
-          : Container(),
-    );
-
-    final eggStep0 = AnimatedOpacity(
+    final eggStep1 = AnimatedOpacity(
       duration: Duration(milliseconds: 700),
       opacity: this.eggBreakStep == 0 ? 1.0 : 0.0,
       child: this.eggBreakStep == 0
@@ -303,96 +226,6 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
               scale: 0.5,
               child: Image.asset(
                 KAssets.IMG_TAMAGO_1,
-                package: 'app_core',
-              ),
-            )
-          : Container(),
-    );
-
-    final eggStep1 = AnimatedOpacity(
-      duration: Duration(milliseconds: 700),
-      opacity: this.showEgg && this.eggBreakStep == 1 ? 1.0 : 0.0,
-      child: this.eggBreakStep == 1
-          ? Transform.scale(
-              alignment: Alignment.bottomCenter,
-              scale: 0.5,
-              child: KImageAnimation(
-                animationType: KImageAnimationType.SHAKE_THE_TOP,
-                imageUrls: [
-                  KAssets.IMG_TAMAGO_1,
-                ],
-                isAssetImage: true,
-                maxLoop: 2,
-                onFinish: () {
-                  Future.delayed(Duration(milliseconds: 750), () {
-                    this.setState(() {
-                      this.eggBreakStep = this.eggBreakStep + 1;
-                    });
-
-                    Future.delayed(Duration(milliseconds: 1000), () {
-                      this.setState(() {
-                        this.eggBreakStep = this.eggBreakStep + 1;
-                      });
-
-                      Future.delayed(Duration(milliseconds: 1000), () {
-                        this.setState(() {
-                          this.eggBreakStep = this.eggBreakStep + 1;
-                        });
-
-                        Future.delayed(Duration(milliseconds: 1500), () {
-                          this.setState(() {
-                            this.eggBreakStep = this.eggBreakStep + 1;
-                            this.showAdult = true;
-                          });
-                        });
-                      });
-                    });
-                  });
-                },
-              ),
-            )
-          : Container(),
-    );
-
-    final eggStep2 = AnimatedOpacity(
-      duration: Duration(milliseconds: 700),
-      opacity: this.showEgg && this.eggBreakStep == 2 ? 1.0 : 0.0,
-      child: this.eggBreakStep == 2
-          ? Transform.scale(
-              alignment: Alignment.bottomCenter,
-              scale: 0.5,
-              child: Image.asset(
-                KAssets.IMG_TAMAGO_2,
-                package: 'app_core',
-              ),
-            )
-          : Container(),
-    );
-
-    final eggStep3 = AnimatedOpacity(
-      duration: Duration(milliseconds: 700),
-      opacity: this.showEgg && this.eggBreakStep == 3 ? 1.0 : 0.0,
-      child: this.eggBreakStep == 3
-          ? Transform.scale(
-              alignment: Alignment.bottomCenter,
-              scale: 0.5,
-              child: Image.asset(
-                KAssets.IMG_TAMAGO_3,
-                package: 'app_core',
-              ),
-            )
-          : Container(),
-    );
-
-    final eggStep4 = AnimatedOpacity(
-      duration: Duration(milliseconds: 700),
-      opacity: this.showEgg && this.eggBreakStep == 4 ? 1.0 : 0.0,
-      child: this.eggBreakStep == 4
-          ? Transform.scale(
-              alignment: Alignment.bottomCenter,
-              scale: 0.5,
-              child: Image.asset(
-                KAssets.IMG_TAMAGO_4,
                 package: 'app_core',
               ),
             )
@@ -434,47 +267,7 @@ class _KEggHeroIntroState extends State<KEggHeroIntro>
           child: Container(
             width: heroWidth * 2,
             height: heroHeight * 2,
-            child: eggStep0,
-          ),
-        ),
-        Align(
-          alignment: Alignment(_barrelMovingAnimation.value, 1),
-          child: Container(
-            width: heroWidth * 2,
-            height: heroHeight * 2,
             child: eggStep1,
-          ),
-        ),
-        Align(
-          alignment: Alignment(_barrelMovingAnimation.value, 1),
-          child: Container(
-            width: heroWidth * 2,
-            height: heroHeight * 2,
-            child: eggStep2,
-          ),
-        ),
-        Align(
-          alignment: Alignment(_barrelMovingAnimation.value, 1),
-          child: Container(
-            width: heroWidth * 2,
-            height: heroHeight * 2,
-            child: eggStep3,
-          ),
-        ),
-        Align(
-          alignment: Alignment(_barrelMovingAnimation.value, 1),
-          child: Container(
-            width: heroWidth * 2,
-            height: heroHeight * 2,
-            child: eggStep4,
-          ),
-        ),
-        Align(
-          alignment: Alignment(_barrelMovingAnimation.value, 1),
-          child: Container(
-            width: heroWidth * 2,
-            height: heroHeight * 2,
-            child: adultView,
           ),
         ),
       ],
