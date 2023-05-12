d9c75577d9ac2a5057f6e8e141b4710fe344eeb5
rkl555 <85212537+rkl550@users.noreply.github.com>
2023-05-12 17:18:48 +0700

    updated analyzer 5.1.2 and json_serializable 6.6.2

--- lib/model/klat_lng.g.dart
+++ lib/model/klat_lng.g.dart
@@ -10,16 +10,7 @@ KLatLng _$KLatLngFromJson(Map<String, dynamic> json) => KLatLng()
   ..lat = double.parse(json['lat'] as String)
   ..lng = double.parse(json['lng'] as String);
 
-Map<String, dynamic> _$KLatLngToJson(KLatLng instance) {
-  final val = <String, dynamic>{};
-
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
-    }
-  }
-
-  writeNotNull('lat', KStringHelper.dtoa(instance.lat));
-  writeNotNull('lng', KStringHelper.dtoa(instance.lng));
-  return val;
-}
+Map<String, dynamic> _$KLatLngToJson(KLatLng instance) => <String, dynamic>{
+      'lat': KStringHelper.dtoa(instance.lat),
+      'lng': KStringHelper.dtoa(instance.lng),
+    };
