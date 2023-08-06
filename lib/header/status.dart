0986ce1d2f529c34cbac77e2948e05ceb93390ef
rkl550 <rkl550@gmail.com>
2023-08-07 04:15:36 +0700

    standard status

---
+++ lib/header/status.dart
@@ -0,0 +1,17 @@
+abstract class Status {
+  static const String TRUE         = "T";
+  static const String FALSE        = "F";
+  static const String YES          = "Y";
+  static const String NO           = "N";
+
+  static const String ACTIVE       = "A";
+  static const String PENDING      = "P";
+  static const String DONE         = "E";
+  static const String ARCHIVE      = "Z";
+  static const String BLOCK        = "B";
+  static const String DELETE       = "X";
+  static const String DISABLE      = "D";
+  static const String INACTIVE     = "I";
+  static const String RESTRICT     = "R";
+  static const String ALERT        = "K";
+}
