cec58c1fef1cc2c78122847c82532c5a7d4d7cba
rkl550 <rkl550@gmail.com>
2023-08-06 15:54:20 +0700

    kaction file

---
+++ lib/header/kaction.dart
@@ -0,0 +1,29 @@
+abstract class KAction {
+  static const String GET = "get";
+  static const String LIST = "list";
+  
+  static const String ADD = "add";
+  static const String CREATE = "create";
+
+  static const String MODIFY = "modify";
+  static const String UPDATE = "update";
+
+  static const String REMOVE = "remove";
+  static const String DELETE = "delete";
+
+  static const String SUBMIT = "submit";
+  static const String APPLY = "apply";
+
+  static const String MARK = "mark";
+  static const String CHECK = "check";
+  static const String UNCHECK = "uncheck";
+
+  static const String DISABLE = "disable";
+  static const String ENABLE = "enable";
+  static const String ACCEPT = "accept";
+  static const String REJECT = "reject";
+
+  static const String LATEST = "latest";
+  static const String NEWEST = "newest";
+  static const String OLDEST = "oldest";
+}
