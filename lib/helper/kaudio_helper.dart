643157ee953af830bf53dd485ac478007b6e84b5
alex-heritier <alex.heritier@gmail.com>
2023-08-10 16:49:39 +0700

    major bug fixes to KAudioHelper - this library should be updated eventually

--- lib/helper/kaudio_helper.dart
+++ lib/helper/kaudio_helper.dart
@@ -1,10 +1,28 @@
+import 'dart:io';
+
+import 'package:app_core/app_core.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
 abstract class KAudioHelper {
   static AudioPlayer _audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
 
   /// Play an audio file
   static Future<int> playAsset(String asset) async {
-    return _audioPlayer.play(asset, isLocal: true);
+    final file = await _getImageFileFromAssets(asset);
+    return _audioPlayer.play(file.path, isLocal: true);
+  }
+
+  static Future<File> _getImageFileFromAssets(String path) async {
+    final byteData = await rootBundle.load(path);
+    final buffer = byteData.buffer;
+    final tempDir = await getTemporaryDirectory();
+    final tempPath = tempDir.path;
+    final filePath = tempPath +
+        "/file_${KUtil.buildRandomString(1)}.tmp"; // file_01.tmp is dump file, can be anything
+    final file = File(filePath);
+    return file.writeAsBytes(
+        buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
   }
 }
