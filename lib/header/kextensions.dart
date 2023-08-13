92bda0e617dea9362aad402945a17976dadd610c
alex-heritier <alex.heritier@gmail.com>
2023-08-13 17:41:13 +0700

    kextensions code cleanup

--- lib/header/kextensions.dart
+++ lib/header/kextensions.dart
@@ -8,27 +8,26 @@ extension KList<T> on Iterable<T> {
     for (int i = 0; i < this.length; i++) {
       result.add(this.elementAt(i));
       final isLastItem = i == (this.length - 1);
-      if (!isLastItem || addToEnd) result.add(t);
+      if (!isLastItem || addToEnd) {
+        result.add(t);
+      }
     }
     return result;
   }
 
   /// Remove duplicates
-  Iterable<T> unique(dynamic Function(T) extractor) =>
-      map(
-              (e) => extractor(e)) // extract IDs
-          .toSet() // remove duplicates
-          .map((e1) =>
+  Iterable<T> unique(dynamic Function(T) extractor) => map(
+          (e) => extractor(e)) // extract IDs
+      .toSet() // remove duplicates
+      .map((e1) =>
           firstWhereOrNull((e2) => e1 == extractor(e2))) // pick 1 matching item
-          .where((e) => e != null) // remove nulls
-          .cast<T>();
+      .where((e) => e != null) // remove nulls
+      .cast<T>();
 }
 
 extension KWidget<T> on State<StatefulWidget> {
   bool isKeyboardVisible() {
-    final insets = MediaQuery
-        .of(this.context)
-        .viewInsets;
+    final insets = MediaQuery.of(this.context).viewInsets;
     return insets.bottom != 0;
   }
 
