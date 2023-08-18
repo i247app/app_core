7f3400f5ab13ed1812bff13c92c105c42901e225
alex-heritier <alex.heritier@gmail.com>
2023-08-18 13:24:06 +0700

    .g file changes

--- lib/model/kstudent.g.dart
+++ lib/model/kstudent.g.dart
@@ -48,8 +48,8 @@ KStudent _$KStudentFromJson(Map<String, dynamic> json) => KStudent()
   ..parentEmail = json['parentEmail'] as String?
   ..parentPhone = json['parentPhone'] as String?
   ..addressLine = json['addressLine'] as String?
-  ..address1 = json['addressLine1'] as String?
-  ..address2 = json['addressLine2'] as String?
+  ..addressLine1 = json['addressLine1'] as String?
+  ..addressLine2 = json['addressLine2'] as String?
   ..city = json['city'] as String?
   ..stateCode = json['stateCode'] as String?
   ..zipCode = json['zipCode'] as String?
@@ -149,8 +149,8 @@ Map<String, dynamic> _$KStudentToJson(KStudent instance) {
   writeNotNull('parentEmail', instance.parentEmail);
   writeNotNull('parentPhone', instance.parentPhone);
   writeNotNull('addressLine', instance.addressLine);
-  writeNotNull('addressLine1', instance.address1);
-  writeNotNull('addressLine2', instance.address2);
+  writeNotNull('addressLine1', instance.addressLine1);
+  writeNotNull('addressLine2', instance.addressLine2);
   writeNotNull('city', instance.city);
   writeNotNull('stateCode', instance.stateCode);
   writeNotNull('zipCode', instance.zipCode);
--- lib/model/tutor.g.dart
+++ lib/model/tutor.g.dart
@@ -48,8 +48,8 @@ Tutor _$TutorFromJson(Map<String, dynamic> json) => Tutor()
   ..parentEmail = json['parentEmail'] as String?
   ..parentPhone = json['parentPhone'] as String?
   ..addressLine = json['addressLine'] as String?
-  ..address1 = json['addressLine1'] as String?
-  ..address2 = json['addressLine2'] as String?
+  ..addressLine1 = json['addressLine1'] as String?
+  ..addressLine2 = json['addressLine2'] as String?
   ..city = json['city'] as String?
   ..stateCode = json['stateCode'] as String?
   ..zipCode = json['zipCode'] as String?
@@ -170,8 +170,8 @@ Map<String, dynamic> _$TutorToJson(Tutor instance) {
   writeNotNull('parentEmail', instance.parentEmail);
   writeNotNull('parentPhone', instance.parentPhone);
   writeNotNull('addressLine', instance.addressLine);
-  writeNotNull('addressLine1', instance.address1);
-  writeNotNull('addressLine2', instance.address2);
+  writeNotNull('addressLine1', instance.addressLine1);
+  writeNotNull('addressLine2', instance.addressLine2);
   writeNotNull('city', instance.city);
   writeNotNull('stateCode', instance.stateCode);
   writeNotNull('zipCode', instance.zipCode);
